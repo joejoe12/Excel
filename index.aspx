a3c5a8aeb505861f46e733c52ee97d14b1cc6740
joejoe12 <joseph.hilou@gmail.com>
2016-04-17 07:18:17 +0300

    hh

---
+++ index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Excel.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="false"     />
+        <asp:Button Text="Load" runat="server" ID="btnLoad" OnClick="btnLoad_Click" />
+        <asp:GridView runat="server" ID="grd1" AutoGenerateColumns="true" >       </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using DashBoard.Excel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Excel
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string Extension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+                if (Extension.StartsWith(".xls"))
+                {
+                    string FileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    string FolderPath = ConfigurationManager.AppSettings["FolderPath"];
+                    string FilePath = Server.MapPath(FolderPath + FileName);
+                    FileUpload1.SaveAs(FilePath);
+                    ImportExcel impExcel = new ImportExcel();
+
+                    using (DataTable dt = impExcel.LoadExcelToDataBase(FilePath,""))
+                    {
+                        grd1.DataSource = dt;
+                        grd1.DataBind();
+                    }
+                } 
+            }
+        }
+    }
+}
